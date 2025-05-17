633d17b8c506fa4d8ea0428f4fe4d979de51fac9
= <koh_zihao_alexander@s2021.ssts.edu.sg>
2025-05-17 22:24:14 +0800

    first commit

---
+++ components/header.js
@@ -0,0 +1,8 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+/* i want to create a header element so it can be replicated across all pages, but ill brush up more on my js first. -alex */ 
+
