5ee6ffbb415ce954d57f5ccaf4117aa384da6472
senkyte <KOH_ZIHAO_ALEXANDER@S2021.SSTS.EDU.SG>
2025-12-01 22:34:08 +0800

    kita kita kita ikuyo!

---
+++ alexscript.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("randomArt")
+let danbooru = document.getElementById("danbooru")
+let url = "https://danbooru.donmai.us/posts.json?tags=kita_ikuyo+rating:safe&limit=1&random=true"
+button.addEventListener('click', function (){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let variants = data[0].media_asset.variants
+        let newurl = variants[2].url
+        danbooru.src = newurl
+    })
+
+
+})
